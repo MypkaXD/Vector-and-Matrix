4b5fe8bfde718516d3d642daa6824e32f1899573
MypkaXD <85798586+MypkaXD@users.noreply.github.com>
2022-05-27 03:10:47 +0300

    Update incMatrix.h

--- incMatrix.h
+++ incMatrix.h
@@ -15,8 +15,8 @@ public:
 	}
 	Matrix(int line, int column) :
 		m_line(line), m_column(column) {
-		m_lines = new Vector[m_line];
-		for (int count = 0; count < m_column; count++) {
+		m_lines = new Vector [m_line];
+		for (int count = 0; count < m_line; count++) {
 			m_lines[count] = Vector(m_column);
 		}
 	}
@@ -67,16 +67,29 @@ public:
 	friend std::ostream& operator<<(std::ostream& out, const Matrix& other) {
 		for (int count = 0; count < other.m_line; count++) {
 			for (int COUNT = 0; COUNT < other.m_column; COUNT++) {
-				out << other.m_lines[count][COUNT] << " ";
+				out << other.m_lines[count][COUNT] << "\t";
 			}
 			out << "\n";
 		}
 		return out;
 	}
 	friend std::istream& operator>>(std::istream& in, Matrix& other) {
+		std::cout << "Введите матрицу: " << other.m_line << " x " << other.m_column << std::endl;
 		for (int count = 0; count < other.m_line; count++) {
 			for (int COUNT = 0; COUNT < other.m_column; COUNT++) {
-				in >> other.m_lines[count][COUNT];
+				bool checker = true;
+				std::cout << "Элемент[" << count << "][" << COUNT << "]: ";
+				while (checker) {
+					in >> other.m_lines[count][COUNT];
+					if (std::cin.fail()) // если предыдущее извлечение оказалось неудачным,
+					{
+						std::cin.clear(); // то возвращаем cin в 'обычный' режим работы
+						std::cin.ignore(32767, '\n'); // и удаляем значения предыдущего ввода из входного буфера
+						std::cout << "Ошибка, введите элемент[" << count << "][" << COUNT << "] заново: ";
+					}
+					else
+						checker = false;
+				}
 			}
 		}
 		return in;
